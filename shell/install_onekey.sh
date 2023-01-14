06df12ba062785dcbd422122221cdcb2b288cf1d
“Frank.ku” <frank.ku@fiadigi.com>
2023-01-14 17:12:25 +0800

    修改文字

--- shell/install_onekey.sh
+++ shell/install_onekey.sh
@@ -23,7 +23,7 @@ PACKAGE_UPDATE=("apt-get -y update" "apt-get -y update" "yum -y update" "yum -y 
 PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
 PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
 
-[[ $EUID -ne 0 ]] && red "注意：请在root用户下运行脚本" && exit 1
+[[ $EUID -ne 0 ]] && red "注意：請在root用户下執行脚本" && exit 1
 
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
@@ -36,8 +36,8 @@ done
 
 back2menu() {
     echo ""
-    green "所选命令操作执行完成"
-    read -rp "请输入“y”退出，或按任意键回到主菜单：" back2menuInput
+    green "所選命令操作執行完成"
+    read -rp "請輸入“y”退出，或按任意鍵回到主菜單：" back2menuInput
     case "$back2menuInput" in
         y) exit 1 ;;
         *) menu ;;
