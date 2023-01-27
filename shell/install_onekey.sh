a1e617657a666734215eb8074ea254bb537dc9a0
fiadigifrank <frank.ku@fiadigi.com>
2023-01-27 18:50:27 +0800

    fix arug

--- shell/install_onekey.sh
+++ shell/install_onekey.sh
@@ -46,17 +46,17 @@ back2menu() {
 
 install_docker_compose(){
     sudo apt-get install \
-     ca-certificates \
-     curl \
-     gnupg \
-     lsb-release -y
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
 
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
     echo \
-       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt-get update -y
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
@@ -118,7 +118,7 @@ k8s_install_crio(){
     sudo apt update
     sudo apt install cri-o cri-o-runc -y
 
-    sudo systemctl start crio && sudo systemctl enable crio -y
+    sudo systemctl start crio && sudo systemctl enable crio
     back2menu
 }
 
